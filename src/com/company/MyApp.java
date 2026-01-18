3a4b6951558efa17d530375e11449585b1caaeb7
Mechta <orazbai.alibek@gmail.com>
2026-01-18 22:07:46 +0500

    merged the changes

--- src/com/company/MyApp.java
+++ src/com/company/MyApp.java
@@ -106,11 +106,4 @@ public class MyApp {
             }
         }
     }
-
-    public void deleteUserMenu() {
-        System.out.println("Please enter id");
-        int id = sc.nextInt();
-        String response = userCont.deleteUser(id);
-        System.out.println(response);
-    }
 }

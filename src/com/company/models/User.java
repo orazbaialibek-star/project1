cda3d03fec231c6b880e971cedb8f1fa11561084
diqtu07-byte <diqtu07@gmail.com>
2026-02-01 22:16:50 +0600

    Update User.java
    Dias

--- src/com/company/models/User.java
+++ src/com/company/models/User.java
@@ -1,84 +1,53 @@
-package com.company.models;
-
 public class User {
-    private int id;
-    private String name;
-    private String surname;
-    private double balance;
-    private String login;
-    private String password;
-    private int role;
 
-    public User(String name, String surname, double balance, String login, String password, int role) {
-        setName(name);
-        setSurname(surname);
-        setBalance(balance);
-        setLogin(login);
-        setPassword(password);
-        setRole(role);
+    private final Long id;
+    private final String username;
+    private final String email;
+    private final String role;
+    private final boolean active;
+
+    private User(Builder builder) {
+        this.id = builder.id;
+        this.username = builder.username;
+        this.email = builder.email;
+        this.role = builder.role;
+        this.active = builder.active;
     }
 
-    public User(int id, String name, String surname, double balance, String login, String password, int role) {
-        this(name, surname, balance, login, password, role);
-        setId(id);
-    }
+    public static class Builder {
+        private Long id;
+        private String username;
+        private String email;
+        private String role;
+        private boolean active;
 
-    public User(int id, String name, String surname, double balance, String login, int role) {
-    }
+        public Builder id(Long id) {
+            this.id = id;
+            return this;
+        }
 
-    public int getId() {
-        return id;
-    }
-    public void setId(int id) {
-        this.id = id;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public String getSurname() {
-        return surname;
-    }
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-    public double getBalance() {
-        return balance;
-    }
-    public void setBalance(double balance) {
-        this.balance = balance;
-    }
-    public int getRole(){
-        return role;
-    }
-    public void setRole(int role){
-        this.role = role;
-    }
+        public Builder username(String username) {
+            this.username = username;
+            return this;
+        }
 
-    public String getLogin(){
-        return login;
-    }
-    public void setLogin(String login){
-        this.login = login;
-    }
-    public String getPassword(){
-        return password;
-    }
-    public void setPassword(String password){
-        this.password = password;
-    }
+        public Builder email(String email) {
+            this.email = email;
+            return this;
+        }
 
-    @Override
-    public String toString(){
-        return "User{" +
-                "id=" + getId() +
-                ", name='" + getName() + '\'' +
-                ", surname='" + getSurname() + '\'' +
-                ", balance=" + getBalance() + '\'' +
-                ", login=" + getLogin() + '\'' +
-                ", role=" + getRole() + '\'' +
-                '}';
+        public Builder role(String role) {
+            this.role = role;
+            return this;
+        }
+
+        public Builder active(boolean active) {
+            this.active = active;
+            return this;
+        }
+
+        public User build() {
+            return new User(this);
+        }
     }
 }

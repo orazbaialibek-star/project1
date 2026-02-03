dd03c60d803dccec53ac03ef7ee4ce3f69d187fa
Mechta <orazbai.alibek@gmail.com>
2026-02-03 18:42:52 +0500

    Fixid createUser method

--- src/com/company/controllers/interfaces/IUserController.java
+++ src/com/company/controllers/interfaces/IUserController.java
@@ -6,6 +6,6 @@ public interface IUserController {
     String getAllUsers();
     String getUser(int id);
     String deleteUser(int id);
-    String createUser(String name, String surname, double balance, String login, String password);
+    String createUser(String name, String surname, double balance, String login, String password, int role);
     String getUsersWithTransactions();
 }

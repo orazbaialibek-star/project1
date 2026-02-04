97e5f6382a5fe9d68847f470bb656023a68a90e1
diqtu07-byte <diqtu07@gmail.com>
2026-02-04 15:01:53 +0500

    .

--- src/com/company/models/Category.java
+++ src/com/company/models/Category.java
@@ -1,5 +1,29 @@
 package com.company.models;
 
 public class Category {
-  
+
+    private int id;
+    private String name;
+
+    public Category(String name) {
+        setName(name);
+    }
+
+    public Category(int id, String name) {
+        this(name);
+        setId(id);
+    }
+
+    public int getId() {return id;}
+    public void setId(int id) {this.id = id;}
+    public String getName() {return name;}
+    public void setName(String name) {this.name = name;}
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + getId() +
+                ", name='" + getName() + '\'' +
+                '}';
+    }
 }
